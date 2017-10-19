4575130106211206f01194b8072d5effdd51fd2f
Programmer Muslim <rizki@imastudio.co.id>
2017-10-19 17:14:44 +0700

    add

--- NewCellCustomCell/NewCellCustomCell/CustomCellTableViewController.swift
+++ NewCellCustomCell/NewCellCustomCell/CustomCellTableViewController.swift
@@ -21,23 +21,16 @@ class CustomCellTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //setiing height tavble view
         tableView.estimatedRowHeight = 650.0
         tableView.rowHeight = UITableViewAutomaticDimension
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
